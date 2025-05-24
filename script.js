5d19d4bad0196da5eb060318dd9b3c3e7aa7f185
FatchurR <rochman.do.id@gmail.com>
2025-05-24 11:26:43 +0700

    add

---
+++ script.js
@@ -0,0 +1,22 @@
+// const codeElement = document.getElementById('code');
+// const codeString = 
+// `public class Introduction {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, my name is Fatchur Rochman.");
+//         System.out.println("I am a Java & Android Developer.");
+//     }
+// }`;
+
+// let index = 0;
+
+// function typeCode() {
+//   if (index < codeString.length) {
+//     codeElement.innerHTML = codeString.substring(0, index) + '<span class="cursor"></span>';
+//     index++;
+//     setTimeout(typeCode, 60);
+//   } else {
+//     codeElement.innerHTML = codeString;
+//   }
+// }
+
+// typeCode();
